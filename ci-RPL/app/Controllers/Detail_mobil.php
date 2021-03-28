8fc03a279c149185eb754a7def3333eecba49bdc
agusnarestha <63548030+agusnarestha@users.noreply.github.com>
2021-03-28 17:51:16 +0800

    detail_mobil

---
+++ ci-RPL/app/Controllers/Detail_mobil.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+class Detail_mobil extends BaseController
+{
+	public function detail_mobil()
+	{
+		echo view('header');
+		echo view('detail_mobil');
+		echo view('footer');
+	}
+}
