68d10fd728ea59a22673dc10ca7d424db0f96172
agusnarestha <63548030+agusnarestha@users.noreply.github.com>
2021-04-01 10:43:31 +0800

    edit database

--- ci-RPL/app/Config/Database.php
+++ ci-RPL/app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
 		'hostname' => 'localhost',
 		'username' => 'root',
 		'password' => '',
-		'database' => 'bemo',
+		'database' => 'bemologin',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
