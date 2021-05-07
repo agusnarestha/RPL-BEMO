ae3a0366680eeafd740cafe7a6e1fa6e8190a953
agusnarestha <63548030+agusnarestha@users.noreply.github.com>
2021-05-07 10:19:09 +0800

    deskripsi tampilUser.php

--- ci-RPL/app/Controllers/tampilUser.php
+++ ci-RPL/app/Controllers/tampilUser.php
@@ -6,24 +6,29 @@ use App\Models\Model_Pembeli;
 
 class tampilUser extends BaseController
 {
-    // protected $user;
-
+    //Menginialisasi objek dari model
     function __construct()
     {
         $this->pembeli = new Model_Pembeli();
     }
-    #Menampilkan tabel pembeli
+    
+    //Fungsi untuk menampilkan data tabel pembeli
     public function index()
     {
-        // $data['user'] = $this->user->showUser();
+        //Menginialisasi data pembeli yang diambil dari model getPembeli()
         $data['pembeli'] = $this->pembeli->getPembeli();
+        //Menampilkan view tabel dan menampilkan data yang diinisialisasi tadi
         echo view('tabel', $data);
     }
 
+    //Fungsi untuk menghapus data pembeli berdasarkan no ktp
     public function deleteUser($no_ktp)
     {
-        $this->user->deleteUser($no_ktp);
+        //Untuk mendelete data pembeli di database
+        $this->db->table('pembeli')->deleteUser($no_ktp);
+        //menampilkan pesan berhasil didelete
         session()->setFlashdata('pesan', 'data berhasil dihapus');
+        //pindah page ke tampilUser jika sudah dilakukan
         return redirect()->to(base_url('tampilUser'));
     }
 }
